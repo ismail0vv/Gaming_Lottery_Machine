d0e2f438c0a25b389feb350f79faf7c614a3ee14
Asylbek <asylismailov05@gmail.com>
2022-08-29 17:50:49 +0600

    first commit

---
+++ Display.py
@@ -0,0 +1,57 @@
+import os
+import time
+
+
+def clear_screen():
+    os.system('cls')
+
+
+class Display:
+
+    def __init__(self, nums):
+        self.__nums = nums
+        self.__lst = [' ', ' ', ' ']
+
+    @property
+    def nums(self):
+        return self.__nums
+
+    @property
+    def lst(self):
+        return self.__lst
+
+    def display(self):
+        print('╔═╦═╦═╗ ▄')
+        print(f'║{self.lst[0]}║{self.lst[1]}║{self.lst[2]}║─┘')
+        print('╚═╩═╩═╝')
+
+    def choosing_number(self, pos, spins):
+        for i in range(spins):
+            self.lst[pos] = self.nums[i % len(self.nums)]
+            self.display()
+            time.sleep(0.05)
+            if i < spins - 1:clear_screen()
+        time.sleep(1)
+        if pos != 2: clear_screen()
+    def count_money(self):
+        amount = 0
+        if self.lst[0] == self.lst[1] and self.lst[0] == self.lst[2]:
+            if self.lst[0] == '1':
+                amount = 1000
+            elif self.lst[0] == '2':
+                amount = 2000
+            elif self.lst[0] == '3':
+                amount = 3000
+            elif self.lst[0] == '4':
+                amount = 4000
+            elif self.lst[0] == '5':
+                amount = 5000
+            else:
+                amount = 50000
+                print('Вы сорвали джекпот')
+
+        print(f'Вы выиграли {amount}$')
+        return amount
+
+    def clear_display(self):
+        self.__lst = [' ', ' ', ' ']
---
+++ Main.py
@@ -0,0 +1,38 @@
+import random
+import os
+import time
+from Display import Display
+
+
+def clear_screen():
+    os.system('cls')
+
+
+def generate_numbers(avtomat):
+    avtomat.choosing_number(0, random.randint(20, 40))
+    avtomat.choosing_number(1, random.randint(20, 40))
+    avtomat.choosing_number(2, random.randint(20, 40))
+
+
+avtomat = Display(['1', '2', '3', '4', '5', '$'])
+money = 10000
+while True:
+    if(money < 1000):
+        print('Недостаточно денег :(')
+        break
+    answer = input(f'Остаток: {money}$\nЗапустить автомат? (-1000$) ')
+    if answer.lower() == 'да':
+        money -= 1000
+        clear_screen()
+        avtomat.clear_display()
+        avtomat.display()
+        time.sleep(1)
+
+        generate_numbers(avtomat)
+
+        clear_screen()
+        money += avtomat.count_money()
+    else:
+        break
+
+print(f'Вы ушли с {money}$')
